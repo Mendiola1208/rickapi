4ac80773209662eaab9b40d06e56304514401ba3
Mendiola1208 <enriquemendiolauriel@gmail.com>
2023-03-23 17:39:11 -0600

    first commit

---
+++ primer-proyecto-react/src/App.js
@@ -0,0 +1,32 @@
+import React ,{ useEffect, useState } from "react";
+import Navbar from "./components/Navbar"
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
+
+function App() {
+
+  const [characters, setCharacters] = useState([]);
+  const initialUrl = "https://rickandmortyapi.com/api/character"
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results))
+      .catch((error) => console.log(error));
+
+  };
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, []);
+
+  return (
+    <>
+      <Navbar brand="Rick and Morty" />
+      <div className="container mt-5">
+        <Pagination/>
+        <Characters characters={characters} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ primer-proyecto-react/src/components/Pagination.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Pagination = () => {
+
+return(
+   <nav>
+    <ul>
+        <li>
+            <button>Previous</button>
+        </li>
+        <li>
+            <button>Next</button>
+        </li>
+    </ul>
+   </nav>
+)
+}
+export default Pagination
